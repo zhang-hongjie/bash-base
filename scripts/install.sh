beb78b2a91ea8a57d0ef0fbc946cbe0286fe8864
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-03 07:31:49 +0200

    fix: build docker image

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,11 +14,11 @@ else
 
 	version=${1}
 	if [[ -z ${version} || ${version} == "latest" ]]; then
-	  latest=$(basename "$(curl -fs -o/dev/null -w '%{redirect_url}' https://github.com/zhang-hongjie/bash-base/releases/latest)")
-	  version=${latest}
+		latest=$(basename "$(curl -fs -o/dev/null -w '%{redirect_url}' https://github.com/zhang-hongjie/bash-base/releases/latest)")
+		version=${latest}
 	fi
 
-  installDir=${installDir}/${version}
+	installDir=${installDir}/${version}
 
 	if [[ ! -f ${installDir}/src/bash-base.sh ]]; then
 		echo "version to install: ${version}"
@@ -27,17 +27,17 @@ else
 		cd "${installDir}" || exit
 
 		if ! wget --no-check-certificate https://github.com/zhang-hongjie/bash-base/archive/"${version}".tar.gz; then
-		    echo "Download the version '${version}' failed"
-		    exit 1
+			echo "Download the version '${version}' failed"
+			exit 1
 		fi
 		tar xzf "${version}".tar.gz -C ./ --strip-components 1
 
-    echo -e "\nFiles in $(pwd):$(ls -la)"
+		echo -e "\nFiles in $(pwd):$(ls -la)"
 
-    if [[ "$2" == "verify" ]]; then
-      echo -e "\nVerifying the installation (docker required):"
-      docker run -it --rm -v "$(pwd)":/bash-base -w /bash-base shellspec/shellspec:master-kcov --shell bash spec/*.sh
-    fi
+		if [[ "$2" == "verify" ]]; then
+			echo -e "\nVerifying the installation (docker required):"
+			docker run -it --rm -v "$(pwd)":/bash-base -w /bash-base shellspec/shellspec:master-kcov --shell bash spec/*.sh
+		fi
 
 		ln -fs "${installDir}"/src/bash-base.sh ${binDir}/bash-base."${version}"
 		ln -fs "${installDir}"/man/bash-base.1 ${manDir}/bash-base."${version}".1
@@ -53,7 +53,7 @@ else
 			echo "so specify a version in your script is recommended"
 		fi
 
-    echo -e "\nInstall successfully."
+		echo -e "\nInstall successfully."
 	fi
 
 	source bash-base."${version}"
