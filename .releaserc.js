678bdae5dfccd76af8b67e465b54a9b4a6849334
Hongjie Zhang <hongjie.zhang@renault.com>
2020-09-08 10:56:02 +0200

    ci: config git plugin to commit generated files when release

--- .releaserc.js
+++ .releaserc.js
@@ -5,7 +5,10 @@ module.exports = {
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git",
+    ["@semantic-release/git", {
+      "assets": ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'src', 'docs', 'man'],
+      "message": "chore(release): ${nextRelease.version} [skip ci]"
+    }],
     ["@semantic-release/exec", {
       "prepareCmd": "docker build -t zhj2074/bash-base . && npm run livedoc"
     }],
